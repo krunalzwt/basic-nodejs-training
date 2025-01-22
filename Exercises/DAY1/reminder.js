658b44bc85739ac6094062d81ec9746f95e67770
krunalzwt <krunal.balas@zealousweb.com>
2025-01-22 13:07:52 +0530

    reminder update

--- Exercises/DAY1/reminder.js
+++ Exercises/DAY1/reminder.js
@@ -4,11 +4,23 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
-rl.question(`Define The Duration :`, (seconds) => {
-    let reminder=setTimeout(()=>{
-        console.log("Drink Water Now!!");
-    },seconds*1000);
-    rl.close();      
-})
-
+rl.question("What is the event? : ", (event) => {
+  function time() {
+    rl.question("Enter the time in seconds: ", (seconds) => {
+      const min_int = 0;
+      let input = Number(seconds);
+      if (!isNaN(input) && input >= min_int) {
+        let reminder = setTimeout(() => {
+          console.log(`${event}`);
+        }, input * 1000);
+        rl.close();  
+      } else {
+        console.log(
+          "Invalid Input!Enter Again"
+        );
+        time();  
+      }
+    });
+  }
+  time();  
+});
