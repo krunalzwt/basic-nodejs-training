992125a2b5b23fb3e6fe5b5a6d7d5c2bfe4d54be
krunalzwt <krunal.balas@zealousweb.com>
2025-01-22 11:17:07 +0530

    d-2 t-2

---
+++ Exercises/DAY2/file_create.js
@@ -0,0 +1,10 @@
+const fs=require('fs');
+fs.writeFileSync('a.txt','hello world ');           //creating the file and adding the text 
+
+
+
+
+// fs.appendFileSync('a.txt','hello world !!');        //add new text in the file
+// fs.writeFileSync('a.txt','wifbhdvsdfgwrbj ');       //overwrite the existing file
+// fs.writeFileSync('b.txt','');                           //creating an empty file
+// fs.unlinkSync('b.txt');                                //deleting a file     
