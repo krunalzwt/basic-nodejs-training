9d47aef87e88329be1a80902506388912ea40147
krunalzwt <krunal.balas@zealousweb.com>
2025-01-23 15:38:39 +0530

    task3 changes

--- Exercises/DAY2/server.js
+++ Exercises/DAY2/server.js
@@ -14,8 +14,13 @@ const myServer = http
   .createServer((req, res) => {
     // Parse query string
     const q = url.parse(req.url, true);
-    // console.log(q);
+    let filepath = path.join(__dirname, "files", q?.query?.name ?? "");
+    let content = q.query.content;
+    let filename = q.query.name;
+    console.log(filename);
+
     switch (req.url.split("?")[0]) {
+      //root
       case "/":
         res.end("This is the home page!!");
         break;
@@ -26,83 +31,105 @@ const myServer = http
           if (err) {
             res.end("Error reading directory");
             return;
+          } else {
+            files.forEach((item) => {
+              if (path.extname(item) == ".txt") {
+                res.write(item + "\n");
+              }
+            });
           }
-          files.forEach((item) => {
-            res.write(item + "\n");
-          });
           res.end();
         });
         break;
 
       // Read specific file
       case "/file":
-        let filepath = path.join(__dirname, "files", q.query.name);
-        // console.log(filepath);
-        fs.readFile(filepath, "utf8", (err, data) => {
-          if (err) {
-            console.log(err);
-            res.statusCode = 500;
-            res.end("Error reading the file");
-            return;
-          }
-          res.writeHead(200, { "Content-Type": "text/plain" });
-          res.write(data);
-          res.end();
-        });
+        if (path.extname(filepath) !== ".txt") {
+          res.end("please enter .txt files");
+        } else if (!fs.existsSync(filepath)) {
+          res.end("file does not exists!");
+        } else {
+          fs.readFile(filepath, "utf8", (err, data) => {
+            if (err) {
+              console.log(err);
+              res.statusCode = 500;
+              res.end("Error reading the file");
+              return;
+            } else {
+              res.writeHead(200, { "Content-Type": "text/plain" });
+              res.write(data);
+              res.end();
+            }
+          });
+        }
+
         break;
 
       //create a file
       case "/create":
-        let filepath1 = path.join(__dirname, "files", q.query.name);
-        let content = q.query.content;
-        // console.log(filepath1);
-        fs.writeFile(filepath1, content, (err, filecontent) => {
-          if (err) {
-            console.log(err);
-            res.statusCode = 500;
-            res.end("Error reading the file");
-            return;
-          }
-          res.writeHead(200, { "Content-Type": "text/plain" });
-          res.write(`file created and data updataed!:${content}`);
-          res.end();
-        });
+        if (fs.existsSync(filepath)) {
+          res.end("file already exists!!");
+        } else if (path.extname(filepath) !== ".txt") {
+          res.end("file needs to be .txt format!!");
+          return;
+        } else {
+          fs.writeFile(filepath, content, (err, filecontent) => {
+            if (err) {
+              console.log(err);
+              res.statusCode = 500;
+              res.end("Error reading the file");
+              return;
+            }
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.write(`file created and data updataed!:${content}`);
+            res.end();
+          });
+        }
         break;
 
       // append into existing file
 
       case "/append":
-        let filepath2 = path.join(__dirname, "files", q.query.name);
-        let content2 = q.query.content;
-        fs.appendFile(filepath2, content2, (err) => {
-          if (err) {
-            console.log(err);
-            res.statusCode = 500;
-            res.end("Error reading the file");
-            return;
-          }
-          res.writeHead(200, { "Content-Type": "text/plain" });
-          res.write(`data updataed!:${content2}`);
-          res.end();
-        });
+        if (!fs.existsSync(filepath)) {
+          res.end("file does not exists!!");
+        } else if (path.extname(filepath) !== ".txt") {
+          res.end("file needs to be .txt format!!");
+          return;
+        }else{
+          fs.appendFile(filepath, content, (err) => {
+            if (err) {
+              console.log(err);
+              res.statusCode = 500;
+              res.end("Error writing the file");
+              return;
+            }
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.write(`data updataed!:${content}`);
+            res.end();
+          });
+        }
         break;
 
       //delete file
 
       case "/delete":
-        let filepath3 = path.join(__dirname, "files", q.query.name);
-        console.log(filepath3);
-        fs.unlink(filepath3,(err) => {
-          if (err) {
-            console.log(err);
-            res.statusCode = 500;
-            res.end("Error deleting the file");
-            return;
-          }
-          res.writeHead(200, { "Content-Type": "text/plain" });
-          res.write("successfully deleted!!");
-          res.end();
-        });
+
+        if(!fs.existsSync(filepath)){
+          res.end("file does not exists!!");
+        }else{
+          fs.unlink(filepath, (err) => {
+            if (err) {
+              console.log(err);
+              res.statusCode = 500;
+              res.end("Error deleting the file");
+              return;
+            }
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.write("successfully deleted!!");
+            res.end();
+          });
+        }
+
         break;
 
       // default
