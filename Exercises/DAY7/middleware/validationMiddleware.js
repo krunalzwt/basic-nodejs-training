b8de8fc2f537c303c352dc65df63b7363e7c4937
krunalzwt <krunal.balas@zealousweb.com>
2025-02-03 17:28:13 +0530

    minor changes

--- Exercises/DAY7/middleware/validationMiddleware.js
+++ Exercises/DAY7/middleware/validationMiddleware.js
@@ -7,10 +7,9 @@ const validation = (schema) => async (req, res, next) => {
 
         return next();
     } catch (error) {
-        // Handle validation errors
         return res.status(400).json({
             type: error.name,
-            message: error.errors || error.message, // Show all validation errors if there are multiple
+            message: error.errors || error.message,  
         });
     }
 };
--- Exercises/DAY7/models/index.js
+++ Exercises/DAY7/models/index.js
@@ -3,12 +3,11 @@ const Users = require("./users");
 const UserProfiles = require("./userProfile");
 const UserImages = require("./userImages");
 
-// Define Associations
+ 
 Users.hasOne(UserProfiles, { foreignKey: "userId", onDelete: "CASCADE" });
 UserProfiles.belongsTo(Users, { foreignKey: "userId", onDelete: "CASCADE" });
 
 Users.hasMany(UserImages, { foreignKey: "userId", onDelete: "CASCADE" });
 UserImages.belongsTo(Users, { foreignKey: "userId", onDelete: "CASCADE" });
 
-// Export models
 module.exports = { sequelize, Users, UserProfiles, UserImages };
