a92588206784d3b197918f38b548b955f170b7f7
krunalzwt <krunal.balas@zealousweb.com>
2025-02-05 12:22:39 +0530

    phase2 completed

--- Exercises/eCommerceApi/src/controllers/productAndCategoriesController.js
+++ Exercises/eCommerceApi/src/controllers/productAndCategoriesController.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const multer = require("multer");
+const uploadsDir = path.join(__dirname, "../../uploads");
 const products = require('../models/productsModel');
 const categories=require('../models/categoriesModel');
 
@@ -17,6 +20,10 @@ const getAllCategories = async (req, res) => {
 const createCategories = async(req, res) => {
     try {
       const { name } = req.body;
+      const existingCategory=await categories.findOne({where:{name}});
+      if(existingCategory){
+        return res.status(403).send('category already exists!!');
+      }
       const result = await categories.create({
         name,
       });
@@ -26,4 +33,116 @@ const createCategories = async(req, res) => {
     }
 };
 
-module.exports={getAllCategories,createCategories}
+const getAllProducts = async (req, res) => {
+  try {
+    const rows = await products.findAll({
+      attributes: ['id', 'name', 'description', 'price', 'stock', 'category_id']
+    });
+    if (rows.length === 0) {
+      return res.status(404).json({ error: "No products found" });
+    }
+    return res.json(rows);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return res.status(500).send("Failed to fetch products.");
+  }
+};
+
+// CREATE PRODUCT...
+// MULTER : FILENAME AND STORAGE FUNCTION
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadsDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${file.originalname}`);
+  },
+});
+
+// MULTER: FILESIZE,EXTENSION AND STORAGE VALIDATIONS
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (!file.mimetype.startsWith("image/")) {
+      return cb(new Error("Only images are allowed!"), false);
+    }
+    cb(null, true);
+  },
+});
+
+const createProduct = async(req, res) => {
+  try {
+    const { name,description,price,stock,category_id} = req.body;
+    const result = await products.create({
+      name,
+      description,
+      price,
+      stock,
+      category_id,
+      image_url:req.file.path
+    });
+    return res.status(201).json(result);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
+
+const getProductById = async (req, res) => {
+  try {
+    const id=req.params.id;
+    const rows = await products.findByPk(id,{
+      attributes: ['id', 'name', 'description', 'price', 'stock', 'category_id']
+    });
+    if (!rows) {
+      return res.status(404).json({ error: "No products found" });
+    }
+    return res.json(rows);
+  } catch (error) {
+    // console.error("Error fetching products:", error);
+    return res.status(500).send("Failed to fetch products.");
+  }
+};
+
+const updateProductById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { name, description, price, stock, category_id } = req.body;
+
+    const updateQuery = {
+      ...(name && { name }),
+      ...(description && { description }),
+      ...(price && { price }),
+      ...(stock && { stock }),
+      ...(category_id && { category_id }),
+      ...(req.file && { image_url: req.file.path }) 
+    };
+
+    if (Object.keys(updateQuery).length === 0) {
+      return res.status(400).json({ error: "No fields to update!" });
+    }
+
+    const [rows] = await products.update(updateQuery, { where: { id } });
+
+    if (rows === 0) {
+      return res.status(404).json({ error: "id is not available!" });
+    }
+
+    return res.json({ message: "Product updated successfully!!", updatedFields: updateQuery });
+  } catch (error) {
+    console.error("Error updating product:", error);
+    return res.status(500).json({ error: "Failed to update product.", details: error.message });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const product = await products.destroy({ where: { id } });
+    res.status(200).send("product deleted successfully!!");
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    return res.status(500).send("Failed to delete product.");
+  }
+};
+
+module.exports={getAllCategories,createCategories,getAllProducts,createProduct,upload,createProduct,getProductById,updateProductById,deleteProduct}
--- Exercises/eCommerceApi/src/routers/productAndCategoriesRoutes.js
+++ Exercises/eCommerceApi/src/routers/productAndCategoriesRoutes.js
@@ -1,13 +1,13 @@
 const express=require('express');
-const { getAllCategories, createCategories } = require('../controllers/productAndCategoriesController');
+const { getAllCategories, createCategories, upload, createProduct, getAllProducts, getProductById, updateProductById, deleteProduct } = require('../controllers/productAndCategoriesController');
 const authorizeAdmin = require('../middleware/authorizeAdmin');
 const { route } = require('./authRoutes');
 const router=express.Router();
 
 router.route('/categories').get(getAllCategories).post(authorizeAdmin,createCategories);
 
-router.route('/products').get().post();
+router.route('/products').get(getAllProducts).post(authorizeAdmin,upload.single('productPicture'),createProduct);
 
-router.route('/products/:id').get().put().delete();
+router.route('/products/:id').get(getProductById).patch(authorizeAdmin,upload.single('productPicture'),updateProductById).delete(authorizeAdmin,deleteProduct);
 
 module.exports=router;
